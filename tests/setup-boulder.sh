2ecf136e490e061eaa1d337b8ab39cb3fd75d638
Nicolas Duchon <nicolas.duchon@gmail.com>
2018-06-12 08:47:12 +0200

    Fix test yet again

--- tests/setup-boulder.sh
+++ tests/setup-boulder.sh
@@ -2,21 +2,23 @@
 
 set -e
 
-SERVER="http://10.77.77.1:4000/directory"
+acme_endpoint="http://boulder:4000/directory"
 
 setup_boulder() {
   # Per the boulder README:
-  nginx_proxy_ip=$(ifconfig nginx-proxy | grep "inet addr:" | cut -d: -f2 | awk '{ print $1}')
+  nginx_proxy_ip="$(ifconfig nginx-proxy | grep "inet addr:" | cut -d: -f2 | awk '{ print $1}')"
 
-  export GOPATH=${HOME?}/go
-  git clone --depth=1 https://github.com/letsencrypt/boulder \
-    $GOPATH/src/github.com/letsencrypt/boulder
+  export GOPATH=${TRAVIS_BUILD_DIR}/go
+  [[ ! -d $GOPATH/src/github.com/letsencrypt/boulder ]] \
+    && git clone --depth=1 https://github.com/letsencrypt/boulder \
+      $GOPATH/src/github.com/letsencrypt/boulder
   cd $GOPATH/src/github.com/letsencrypt/boulder
   sed --in-place 's/ 5002/ 80/g' test/config/va.json
   sed --in-place 's/ 5001/ 443/g' test/config/va.json
   docker-compose build --pull
   docker-compose run \
     --use-aliases \
+    --name boulder \
     -e FAKE_DNS=$nginx_proxy_ip \
     --service-ports \
     boulder &
@@ -25,13 +27,13 @@ setup_boulder() {
 
 wait_for_boulder() {
   i=0
-  until curl ${SERVER?} >/dev/null 2>&1; do
+  until docker exec boulder bash -c "curl ${acme_endpoint:?} >/dev/null 2>&1"; do
     if [ $i -gt 300 ]; then
       echo "Boulder has not started for 5 minutes, timing out."
       exit 1
     fi
     i=$((i + 5))
-    echo "$SERVER : connection refused. Waiting."
+    echo "$acme_endpoint : connection refused, Boulder isn't ready yet. Waiting."
     sleep 5
   done
 }
--- tests/test.sh
+++ tests/test.sh
@@ -26,10 +26,6 @@ yq write --inplace docker-compose.yaml \
   services.letsencrypt.environment[+] \
   ACME_TOS_HASH=b16e15764b8bc06c5c3f9f19bc8b99fa48e7894aa5a6ccdad65da49bbf564793
 
-yq write --inplace docker-compose.yaml \
-  services.letsencrypt.extra_hosts[+] \
-  "boulder:${boulder_ip}"
-
 yq read docker-compose.yaml
 
 docker-compose -p nginx-proxy up -d
@@ -45,7 +41,8 @@ docker run --rm --detach \
 
 wait_for_cert $test_domain $letsencrypt_container_name
 
-created_cert="$(docker exec $letsencrypt_container_name openssl x509 -in /etc/nginx/certs/$test_domain/cert.pem -text -noout)"
+created_cert="$(docker exec "$letsencrypt_container_name" openssl x509 -in "/etc/nginx/certs/$test_domain/cert.pem" -text -noout)"
+created_cert_fingerprint="$(docker exec "$letsencrypt_container_name" openssl x509 -in "/etc/nginx/certs/$test_domain/cert.pem" -fingerprint -noout)"
 
 if grep -q "$test_domain" <<< "$created_cert"; then
   echo "$test_domain is on certificate."
@@ -56,13 +53,17 @@ fi
 
 wait_for_conn $test_domain
 
-served_cert="$(echo \
+served_cert_fingerprint="$(echo \
   | openssl s_client -showcerts -servername $test_domain -connect $test_domain:443 2>/dev/null \
-  | openssl x509 -inform pem -text -noout)"
+  | openssl x509 -fingerprint -noout)"
 
-if [ "$created_cert" != "$served_cert" ]; then
+if [ "$created_cert_fingerprint" != "$served_cert_fingerprint" ]; then
   echo "Nginx served an incorrect certificate for $test_domain."
-  diff -u <"$(echo "$created_cert")" <"$(echo "$served_cert")"
+  served_cert="$(echo \
+        | openssl s_client -showcerts -servername "$test_domain" -connect "$test_domain:443" 2>/dev/null \
+        | openssl x509 -text -noout \
+        | sed 's/ = /=/g' )"
+  diff -u <"$(echo "$created_cert" | sed 's/ = /=/g')" <"$(echo "$served_cert")"
   exit 1
 else
   echo "The correct certificate for $test_domain was served by Nginx."
